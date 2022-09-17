9c434528771f3c971e84a4381f86848dc83bb80c
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-09-17 04:00:49 +0300

    add more logging stuff

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,36 +5,36 @@ export default (event: string, logData: any) => {
     case 'startup':
       if (!logData.port) {
         console.error(
-          chalk.red(`[ERR] logData.port not set in log event startup`),
+          chalk.red('[ERR] [unsetData] logData.port not set in log event startup'),
         );
         break;
       }
 
       console.log(
-        `Started server at ${chalk.green(`http://0.0.0.0:${logData.port}`)}`,
+        `[LOG] [startup] Started server at ${chalk.green(`http://0.0.0.0:${logData.port}`)}`,
       );
       break;
     case 'request':
       if (!logData.method) {
         console.error(
-          chalk.red(`[ERR] logData.method not set in log event request`),
+          chalk.red('[ERR] [unsetData] logData.method not set in log event request'),
         );
         break;
       }
 
       if (!logData.route) {
         console.error(
-          chalk.red(`[ERR] logData.route not set in log event request`),
+          chalk.red('[ERR] [unsetData] logData.route not set in log event request'),
         );
         break;
       }
 
       console.log(
-        `${chalk.green(logData.method)} ${chalk.yellow(logData.route)}`,
+        `[LOG] [request] ${chalk.green(logData.method)} ${chalk.yellow(logData.route)}`,
       );
       break;
     default:
-      console.error(chalk.red(`[ERR] Unknown log event ${event}`));
+      console.error(chalk.red(`[ERR] [unknownEvent] Unknown log event ${event}`));
       break;
   }
 };
